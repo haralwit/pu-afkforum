e2be001ae433e699bd4680676f94239b1347951b
Harald <haralwit@stud.ntnu.no>
2020-02-21 08:21:48 +0100

    delete extra project in Kode folder

--- Kode/djangopu/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\harald\projects\puproject\kode\djangopu\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
