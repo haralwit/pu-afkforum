8fce8b26cf2f32fbc496f779a7b5d274ee63613e
Harald <haralwit@stud.ntnu.no>
2020-02-24 17:57:55 +0100

    add djang-voting package in post model

--- Kode/django_PU_project/blog/models.py
+++ Kode/django_PU_project/blog/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from vote.models import VoteModel
 
-class Post(models.Model):
+class Post(VoteModel, models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    num_vote = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
