fe43482f746be46ac6ed87b780c81b7fb4dac9e3
Hanne K. Ødegård <hanne.1998@live.no>
2020-03-18 09:41:40 +0100

    Add

--- Kode/django_PU_project/users/urls.py
+++ Kode/django_PU_project/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='users-register'),
-    path('profile/', views.profile, name='users-profile'),
+    path("register/", views.register, name="users-register"),
+    path("profile/", views.profile, name="users-profile"),
 ]
