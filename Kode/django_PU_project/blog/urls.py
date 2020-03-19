2e44d5b4573ba05cdabe0408bdb1c5d5cbc344c6
livehe <livehe@stud.ntnu.no>
2020-03-19 23:02:19 +0100

    Add upvote/downvote (#8)

--- Kode/django_PU_project/blog/urls.py
+++ Kode/django_PU_project/blog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
+
+from updown.views import AddRatingFromModel
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
@@ -9,4 +12,10 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
+    # path('post/<int:pk>/rate/<int:score>', AddRatingFromModel(), name="post-rating"), 
+    re_path(r'^post/(?P<object_id>\d+)/rate/(?P<score>[\d\-]+)$', AddRatingFromModel(), {
+        'app_label': 'blog',
+        'model': 'Post',
+        'field_name': 'rating',
+    }, name="post-rating"),
 ]
