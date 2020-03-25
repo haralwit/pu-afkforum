a6923050e85d3075865032369605ac2966f4093e
livehe <livehe@stud.ntnu.no>
2020-03-25 10:57:28 +0100

    Changed " to ' to be consistent

--- Kode/django_PU_project/users/urls.py
+++ Kode/django_PU_project/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import adminCheck
 
 urlpatterns = [
-    path("register/", views.register, name="users-register"),
-    path("profile/", views.profile, name="users-profile"),
-    path("checkforadmin/", adminCheck, name='admin-check'),
+    path('register/', views.register, name='users-register'),
+    path('profile/', views.profile, name='users-profile'),
+    path('checkforadmin/', adminCheck, name='admin-check'),
 ]
